56655671c483dd95fdde1e8b5edd94bd85889cf5
Bosch <bosch@pinchtab.com>
2026-03-19 10:39:06 +0100

    style: fix prettier formatting on types/index.ts

--- dashboard/src/types/index.ts
+++ dashboard/src/types/index.ts
@@ -99,7 +99,12 @@ export interface BackendProfilesConfig {
 }
 
 export interface BackendMultiInstanceConfig {
-  strategy: "simple" | "explicit" | "simple-autorestart" | "always-on" | "no-instance";
+  strategy:
+    | "simple"
+    | "explicit"
+    | "simple-autorestart"
+    | "always-on"
+    | "no-instance";
   allocationPolicy: "fcfs" | "round_robin" | "random";
   instancePortStart: number;
   instancePortEnd: number;
